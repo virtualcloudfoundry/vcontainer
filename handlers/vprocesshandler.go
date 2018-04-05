4c419b7c64b0bc332fe9f5f66ef4a92dd8e6b877
Andy Liu <andyliuliming@outlook.com>
2018-04-05 20:48:30 +0800

    wait.

--- handlers/vprocesshandler.go
+++ handlers/vprocesshandler.go
@@ -1,8 +1,7 @@
 package handlers
 
 import (
-	"io"
-
+	"github.com/virtualcloudfoundry/vcontainer/interop"
 	"github.com/virtualcloudfoundry/vcontainercommon"
 	"google.golang.org/grpc/metadata"
 
@@ -29,25 +28,55 @@ func (v *vprocessHandler) Signal(context.Context, *vcontainermodels.SignalReques
 
 func (v *vprocessHandler) Wait(empty *google_protobuf.Empty, server vcontainermodels.VProcess_WaitServer) error {
 	// see whether there's one file.
+	// mount the container
+
+	var containerInterop interop.ContainerInterop
+
+	containerId, err := v.getContainerId(server.Context())
+	if err != nil {
+		return err
+	}
+
+	containerInterop = interop.NewContainerInterop(containerId, v.logger)
+	if err = containerInterop.Open(); err != nil {
+		v.logger.Error("vcontainer-stream-in-container-interop-failed-to-open", err)
+		return verrors.New("failed to open container interop.")
+	}
+	defer containerInterop.Close()
+
 	for {
 		err := server.Send(&vcontainermodels.WaitResponse{
 			Exited:   false,
 			ExitCode: -1,
 		})
 		if err != nil {
-			if err != io.EOF {
-				v.logger.Error("vprocess-wait-recv-failed", err)
-				return verrors.New("recv failed.")
-			} else {
-				break
-			}
+			v.logger.Error("vprocess-wait-recv-failed", err)
+			break
+			// if err != io.EOF {
+			// 	v.logger.Error("vprocess-wait-recv-failed", err)
+			// 	return verrors.New("recv failed.")
+			// } else {
+			// 	break
+			// }
 		}
 
 	}
 	return verrors.New("Not implemented")
 }
 
-func (v *vcontainerHandler) getProcessId(ctx context.Context) (string, error) {
+func (v *vprocessHandler) getContainerId(ctx context.Context) (string, error) {
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		return "", verrors.New("context not correct.")
+	}
+	containerID := md[vcontainercommon.ContainerIDKey]
+	if containerID == nil || len(containerID) == 0 {
+		return "", verrors.New("no container id in context.")
+	}
+	return containerID[0], nil
+}
+
+func (v *vprocessHandler) getProcessId(ctx context.Context) (string, error) {
 	md, ok := metadata.FromContext(ctx)
 	if !ok {
 		return "", verrors.New("context not correct.")
